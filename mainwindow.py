2b4d26873a507ced37a658c0d9f1a93a8a6e87f9
rsgrava <rsgrava@hotmail.com>
2023-09-08 15:38:33 -0300

    project: prototype project

--- mainwindow.py
+++ mainwindow.py
@@ -1,16 +1,41 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QInputDialog, QMessageBox
 
 from ui_main import Ui_MainWindow
+from project import Project
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setWindowTitle("lumiedit")
+        self.project = None
 
+        self.ui.new_project_btn.clicked.connect(self.new_project)
+        self.ui.load_project_btn.clicked.connect(self.load_project)
+        self.ui.new_tileset_btn.clicked.connect(self.new_tileset)
+
+    def new_project(self):
+        name, ok = QInputDialog().getText(self, "Insert Project Name", "Project name:")
+        if ok and name:
+            self.project = Project(name)
+            self.setWindowTitle("lumiedit - " + name)
+            self.ui.project_label.setText("Current project: " + name)
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Invalid project name!")
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+    def load_project(self):
+        ...
+
+    def new_tileset(self):
+        ...
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ project.py
@@ -0,0 +1,5 @@
+# This Python file uses the following encoding: utf-8
+
+class Project:
+    def __init__(self, name):
+        self.name = name
